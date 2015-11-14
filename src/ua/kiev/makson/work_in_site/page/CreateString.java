4f3baac3e206efc216bee402b0fe1c12d00c2766
MaximDzhezhelo <makson2007.86@gmail.com>
2015-11-14 11:45:31 +0200

    added class that parses the page description

--- src/ua/kiev/makson/work_in_site/page/CreateString.java
+++ src/ua/kiev/makson/work_in_site/page/CreateString.java
@@ -1,19 +1,57 @@
 package ua.kiev.makson.work_in_site.page;
 
+import java.io.File;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import ua.kiev.makson.work_in_site.FileRead;
+
 public class CreateString {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        String str = "Дом с призраками / House Hunting (Эрик Херт)";
-        int x = str.indexOf('/');
-        System.out.println(x);
-        String strFirst = str.substring(0, x);
-        System.out.println(strFirst);
-        String strSecond = str.substring(x + 1, str.length());
-        System.out.println(strSecond);
+        FileRead fileRead = new FileRead("windows-1251");
+        File file = new File("site.html");
+        if (file.exists()) {
+            String page = fileRead.readFromRootDirectory(file);
 
+            Document doc = Jsoup.parse(page);
+
+            CreateString create = new CreateString();
+            Element body = doc.body();
+
+            System.out.println(create.getIMG(body));
+            System.out.println(create.getDescription(body));
+
+        }
     }
 
+    public String getIMG(Element body) {
+        Element img = body.getElementById("postImgAligned");
+        String imgURL = img.attr("src");
+        return imgURL;
+    }
+
+    public String getDescription(Element body) {
+        StringBuilder sb = new StringBuilder();
+        Elements el = body.getElementsByClass("field");
+        for (Element element : el) {
+            String box = element.ownText();
+            sb.append(box);
+            String descr = element.nextSibling().toString();
+            if (box.equals("Описание:")) {
+                descr = getDescrLongText(body);
+            }
+            descr = String.format("%s%n", descr);
+            sb.append(descr);
+        }
+        return sb.toString();
+    }
+
+    private String getDescrLongText(Element body) {
+        Elements opisSPAN = body.select("span[style=color: darkgreen]");
+        return opisSPAN.text();
+    }
 }
