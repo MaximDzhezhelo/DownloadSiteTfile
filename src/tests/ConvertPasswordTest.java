31ec8ac0f0ba7c7ff3369a89e8ae2a245718bfff
MaximDzhezhelo <makson2007.86@gmail.com>
2015-12-12 23:02:58 +0200

    create new text fields in panel WorkInSitePanel. fixed independence in POM

--- src/tests/ConvertPasswordTest.java
+++ src/tests/ConvertPasswordTest.java
@@ -8,59 +8,59 @@ import org.junit.Test;
 import ua.kiev.makson.controller.ConvertPassword;
 
 public class ConvertPasswordTest {
-    ConvertPassword convert;
+	ConvertPassword convert;
 
-    @Before
-    public void setUp() throws Exception {
-        convert = new ConvertPassword();
-    }
+	@Before
+	public void setUp() throws Exception {
+		convert = new ConvertPassword();
+	}
 
-    @Test
-    public void testPassNull() {
-        char[] pass = null;
-        String result = convert.getPassword(pass);
-        assertNull(result);
-    }
+	@Test
+	public void testPassNull() {
+		char[] pass = null;
+		String result = convert.getPassword(pass);
+		assertNull(result);
+	}
 
-    @Test
-    public void testPassLetters() {
-        char[] pass = { 'a', 'b', 'c', 'd', 'e' };
-        String result = convert.getPassword(pass);
-        assertTrue(result.equals("abcde"));
-    }
+	@Test
+	public void testPassLetters() {
+		char[] pass = { 'a', 'b', 'c', 'd', 'e' };
+		String result = convert.getPassword(pass);
+		assertTrue(result.equals("abcde"));
+	}
 
-    @Test
-    public void testPassLetter() {
-        char[] pass = { 'a' };
-        String result = convert.getPassword(pass);
-        assertTrue(result.equals("a"));
-    }
+	@Test
+	public void testPassLetter() {
+		char[] pass = { 'a' };
+		String result = convert.getPassword(pass);
+		assertTrue(result.equals("a"));
+	}
 
-    @Test
-    public void testPassDash() {
-        char[] pass = { '-' };
-        String result = convert.getPassword(pass);
-        assertTrue(result.equals("-"));
-    }
+	@Test
+	public void testPassDash() {
+		char[] pass = { '-' };
+		String result = convert.getPassword(pass);
+		assertTrue(result.equals("-"));
+	}
 
-    @Test
-    public void testPassSixDash() {
-        char[] pass = { '-', '-', '-', '-', '-', '-' };
-        String result = convert.getPassword(pass);
-        assertTrue(result.equals("------"));
-    }
+	@Test
+	public void testPassSixDash() {
+		char[] pass = { '-', '-', '-', '-', '-', '-' };
+		String result = convert.getPassword(pass);
+		assertTrue(result.equals("------"));
+	}
 
-    @Test
-    public void testPassLettersDash() {
-        char[] pass = { '-', 'w', '-', 'e', '-', 'a' };
-        String result = convert.getPassword(pass);
-        assertTrue(result.equals("-w-e-a"));
-    }
+	@Test
+	public void testPassLettersDash() {
+		char[] pass = { '-', 'w', '-', 'e', '-', 'a' };
+		String result = convert.getPassword(pass);
+		assertTrue(result.equals("-w-e-a"));
+	}
 
-    @Test
-    public void testPassNumbersDash() {
-        char[] pass = { '-', '1', '-', '2', '-', '3' };
-        String result = convert.getPassword(pass);
-        assertTrue(result.equals("-1-2-3"));
-    }
+	@Test
+	public void testPassNumbersDash() {
+		char[] pass = { '-', '1', '-', '2', '-', '3' };
+		String result = convert.getPassword(pass);
+		assertTrue(result.equals("-1-2-3"));
+	}
 }
--- src/ua/kiev/makson/gui/panel/WorkInSitePanel.java
+++ src/ua/kiev/makson/gui/panel/WorkInSitePanel.java
@@ -1,61 +1,89 @@
 package ua.kiev.makson.gui.panel;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import ua.kiev.makson.controller.Controller;
 import ua.kiev.makson.gui.MyFrame;
 
 public class WorkInSitePanel extends JPanel implements ActionListener {
 
-    private static final long serialVersionUID = 1L;
-    private JPanel panel;
-    private JButton butStart;
-    private JButton butStop;
-    private MyFrame frame;
-    private Controller control;
+	private static final long serialVersionUID = 1L;
+	private JPanel panel;
+	private JPanel panelBut;
+	private JPanel panelText;
+	private JButton butStart;
+	private JButton butStop;
+	private JTextField field1;
+	private JTextField field2;
+	private JTextField field3;
+	private MyFrame frame;
+	private Controller control;
 
-    public WorkInSitePanel(MyFrame frame) {
-        this.frame = frame;
-    }
+	public WorkInSitePanel(MyFrame frame) {
+		this.frame = frame;
+	}
 
-    public void createPanelWorkInSitePanel() {
-        createPanel();
-    }
+	public void createPanelWorkInSitePanel() {
+		createPanel();
+	}
 
-    public void createPanel() {
-        panel = new JPanel();
-        createButton();
-        this.add(panel);
+	public void createPanel() {
+		panel = new JPanel();
+		BorderLayout df = new BorderLayout();
+		panel.setLayout(df);
+		createButton();
+		createTextFieldPanel();
+		panel.add(panelBut, BorderLayout.CENTER);
+		panel.add(panelText, BorderLayout.AFTER_LAST_LINE);
+		this.add(panel);
 
-    }
+	}
 
-    public void createButton() {
-        butStart = new JButton("Start");
-        butStart.addActionListener(this);
-        butStop = new JButton("Stop");
-        butStop.addActionListener(this);
-        panel.add(butStart);
-        panel.add(butStop);
-    }
+	public void createButton() {
+		panelBut = new JPanel();
+		butStart = new JButton("Start");
+		butStart.addActionListener(this);
+		butStop = new JButton("Stop");
+		butStop.addActionListener(this);
+		panelBut.add(butStart);
+		panelBut.add(butStop);
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e == null) {
-            return;
-        }
-        if (e.getSource() instanceof JButton) {
-            JButton buton = (JButton) e.getSource();
-            if (buton.getText().equals("Start")) {
-                control = frame.getControl();
-                control.goToTheSite();
-                frame.setControl(control);
-            } else if (buton.getText().equals("Stop")) {
-                control = frame.getControl();
-                control.stopToTheSite();
-                frame.setControl(control);
-            }
-        }
-    }
+	public void createTextFieldPanel() {
+		panelText = new JPanel();
+		field1 = new JTextField(10);
+		field1.setEnabled(false);
+		field2 = new JTextField(10);
+		field2.setEnabled(false);
+		field3 = new JTextField(10);
+		field3.setEnabled(false);
+		panelText.add(field1);
+		panelText.add(field2);
+		panelText.add(field3);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e == null) {
+			return;
+		}
+		if (e.getSource() instanceof JButton) {
+			JButton buton = (JButton) e.getSource();
+			if (buton.getText().equals("Start")) {
+				control = frame.getControl();
+				control.goToTheSite();
+				frame.setControl(control);
+			} else if (buton.getText().equals("Stop")) {
+				control = frame.getControl();
+				control.stopToTheSite();
+				frame.setControl(control);
+			}
+		}
+	}
 }
